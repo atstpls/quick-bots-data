35abcd00144420fb49ab9486c77faa211b0e3692
atstpls <25083331+atstpls@users.noreply.github.com>
2020-05-20 23:25:18 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if command -v git > /dev/null;then echo [+] git installed;else echo "[-] git not installed...exiting" && exit;fi
+if command -v docker > /dev/null;then echo [+] docker installed;else echo "[-] docker not installed...exiting" && exit;fi
+if command -v docker-compose > /dev/null;then echo [+] docker-compose installed;else echo "[-] docker-compose not installed...exiting" && exit;fi
+
+wget https://s3.amazonaws.com/botsdataset/botsv1/splunk-pre-indexed/botsv1_data_set.tgz -O apps/botsv1_data_set.tgz
+cd apps
+
+for i in $(ls *.tgz);do tar -xvf $i;done
+
+git clone https://github.com/splunk/SA-ctf_scoreboard.git
+git clone https://github.com/splunk/SA-ctf_scoreboard_admin.git
